dfb0366896e255efdfa3f4b07d5e4c2006395842
hisaj <jseun7@gmail.com>
2016-03-23 15:26:42 +0000

    Update Payment.java

--- implementation/Model/Payment.java
+++ implementation/Model/Payment.java
@@ -1,41 +1,43 @@
 
 import java.util.*;
-
 /**
  * 
  */
 public class Payment {
-
+    
+   private String currency;
+    private int type;
+    private double weight;
+    
     /**
      * Default constructor
      */
+    public Payment() {}
+    
     public Payment() {
+        this.currency;
+        this.type;
+        this.weight;
+    }
+    
+    public void convert(double weight) {
+       int aValue;
+       if(this.type = 1){
+           switch(aValue){
+               case 1: this.currency = "1"; break;
+               case 2: this.currency = "2"; break;
+               case 3: this.currency = "3"; break;
+               case 4: this.currency = "4"; break;
+           }
+       }
+       else if(this.type = 2){
+           this.currency = readCard();
+       }
     }
 
-    /**
-     * 
-     */
-    private String currency;
-
-    /**
-     * 
-     */
-    private int type;
-
-    /**
-     * 
-     */
-    private double weight;
-
-    /**
-     * @param double 
-     * @return
-     */
-    public void convert(void double) {
-        // TODO implement here
-        return null;
+    public void readCard(){
+        int []array = new int [16];
     }
-
     /**
      * @return
      */
