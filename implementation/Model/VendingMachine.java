519c6bc142827b222477197f99fde7f47fe9eba4
hisaj <jseun7@gmail.com>
2016-03-28 20:36:34 +0100

    Update VendingMachine.java

--- implementation/Model/VendingMachine.java
+++ implementation/Model/VendingMachine.java
@@ -1,84 +1,92 @@
-
 import java.util.*;
 
-/**
- * 
- */
-public class VendingMachine extends System {
+public class VendingMachine  extends Systems { 
     
-    private Slot slots[ ];
+    private Slot slots[];
     /**
      * Default constructor
      */
     public VendingMachine() {}
     
     public VendingMachine(Slot [] slots) {
-         slots = new slots []{};
-    /**
-     * @return
-     */
+         slots = new Slot[20];
+         //Slot tmp; 
+         for(int i=0;i<slots.length;i++){
+        	//tmp = new Slot();
+        	 slots[i]= null; 
+        	 slots[i].setUsed(false);	 
+         }
+    }
+   
     public void startInterface() {
        boolean begin = false;
        if(!begin){
-           listenConnect();
+            listenConnect();
        }
     }
 
-    /**
-     * @param Slot
-     */
-    public void setSlot(Slot mySlot) {
-       this.slots = mySlot;
-    }
-
-    /**
-     * @return
-     */
-    public Slot getSlot() {
-       return this.slots;
-    }
-
-    /**
-     * @return
-     */
-    public abstract void reorder();
-
-    /**
-     * @return
-     */
-    public abstract void errorMessage();
-         System.out.print("This activity was not successful");
-    /**
-     * @param String String 
-     * @return
-     */
-    public abstract boolean validateLogin( String n){
-        
-    }
-        
-    /**
-     * @param String String 
-     * @return
-     */
-    public abstract void setLoginDetails(void String String);
-
-    /**
-     * @return
-     */
-    public abstract void listenConnect();
-    {
-       boolean yesConnect = true;
-    }
+    public void setSlot(int slotIndex, double productWeight, double slotTemperature, Product aProduct){
+    	Product prod;
+    	int i;
+		for(i= 0; i < slots.length; i++ ){
+			
+			prod = new Product(aProduct.getName(), aProduct.getPrice(), aProduct.getCurrencyType(), aProduct.getWeight());	
+			slots[i].setLocation(slotIndex);
+			slots[i].setWeight(productWeight);
+			slots[i].setTemperature(slotTemperature);
+			slots[i].getProduct(prod);
+			slots[i].setUsed(true);
+		}	
+		slots = getSlot();	
+	}
     
-    /**
-     * @return
-     */
-    public abstract double getConfig();
-     
-    /**
-     * @param String 
-     * @return
-     */
-    public abstract void setConfig(void String);
+    public Slot [] getSlot() {
+    	
+       	for(Slot s: slots){
+       		s.getProduct();
+    	}
+    	  return  slots;
+    }
 
+	@Override
+	public void reorder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void errorMessage() {
+		System.out.print("This activity was not successful");
+		
+	}
+
+	@Override
+	public boolean validateLogin(String aString) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setLoginDetails(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listenConnect() {
+		//boolean yesConnect = true;
+		
+	}
+
+	@Override
+	public double getConfig() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setConfig(String n) {
+		// TODO Auto-generated method stub
+		
+	}
 }
